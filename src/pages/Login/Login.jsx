1159fec7912bbd584f6d7eacbbbca4c897f9751a
faijalfshaikh <faijalfshaikh@gmail.com>
2026-05-25 14:19:17 +0530

    Finished Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import "./Login.css"
+import assets from "../../assets/assets"
 
 const Login = () => {
+
+    
+
+
   return (
-    <div>Login</div>
+    <div className='login'> 
+        <img src={assets.logo_big} alt="Logo" className="logo"/>
+        <form className="login-form">
+            <h2>Sign Up</h2>
+            <input type="text" placeholder='Username' className='form-input' required/>
+            <input type="email" placeholder='Email address' className='form-input' required/>
+            <input type="password" placeholder='Password' className='form-input' required/>
+            <button type="submit">Create account</button>
+            <div className='login-term'>
+                <input type='checkbox' id='terms'></input>
+                <p>Agree to the terms of use & privacy policy</p>
+            </div>
+            <div className='login-forgot'>
+                <p className='login-toggle'>Already have an account? <span>Login</span></p>
+            </div>
+        </form>
+    </div>
   )
 }
 
